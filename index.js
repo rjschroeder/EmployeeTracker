09a0f8725362322ecf057809731290e3293aaba0
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-03 14:37:01 -0600

    wrote main menu questions in an array

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const inquirer = require("inquirer");
+const { default: Choices } = require("inquirer/lib/objects/choices");
+
+const questions = [
+    {
+        name: "mainMenu",
+        message: "What would you like to access?",
+        type: "list",
+        choices: [
+            {
+                value: "viewDepts",
+                name: "View all Departments"
+            },
+            {
+                value: "viewRoles",
+                name: "View all Roles"
+            },
+            {
+                value: "viewEmpls",
+                name: "View all Employees"
+            },
+            {
+                value: "addDept",
+                name: "Add a Department"
+            },
+            {
+                value: "addRole",
+                name: "Add a Role"
+            },
+            {
+                value: "addEmpl",
+                name: "Add an Employee"
+            },
+            {
+                value: "updateEmpl",
+                name: "Update an Employee Role"
+            }
+        ]
+    }
+];
