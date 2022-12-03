cf2edea7bd94fcc6c40c3920e70e9e32857cc798
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-03 14:07:36 -0600

    added seeds for role

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,4 +5,15 @@ VALUES  ("Sales"),
         ("Service"),
         ("Engineers")
 
-INSERT INTO role ()
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Lead Salesperson", 450000, 1),
+        ("Junior Salesperson", 230000, 1),
+        ("Training Salesperson", 150000, 1),
+        ("Lead Manager", 970000, 2),
+        ("Manager", 800000, 2),
+        ("Harvard Intern", 30000, 3),
+        ("Yale Intern", 40000, 3),
+        ("Service Worker", 340000, 4),
+        ("Lead Engineer", 900000, 5),
+        ("Senior Engineer", 760000, 5),
+        ("Junior Engineer", 450000, 5)
