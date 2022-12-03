0d773856e078899ae05c6c107e6e3663fef60f65
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-03 13:48:52 -0600

    created department table schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS companyEmployees;
 CREATE DATABASE companyEmployees;
 
 USE companyEmployees;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
