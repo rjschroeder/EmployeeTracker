7b1dbab6031c87048c4faafdf89e4a103308ada4
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-03 13:56:26 -0600

    created schema for employee

--- db/schema.sql
+++ db/schema.sql
@@ -15,4 +15,16 @@ CREATE TABLE role (
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
-)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL,
+);
