34877df51ad8ad27adb160908497aecb7af40986
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-03 14:24:01 -0600

    added seeds for employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ VALUES  ("Sales"),
 
 INSERT INTO role (title, salary, department_id)
 VALUES  ("Lead Salesperson", 450000, 1),
-        ("Junior Salesperson", 230000, 1),
+        ("Senior Salesperson", 230000, 1),
         ("Training Salesperson", 150000, 1),
         ("Lead Manager", 970000, 2),
         ("Manager", 800000, 2),
@@ -17,3 +17,23 @@ VALUES  ("Lead Salesperson", 450000, 1),
         ("Lead Engineer", 900000, 5),
         ("Senior Engineer", 760000, 5),
         ("Junior Engineer", 450000, 5)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("John", "Ham", 4, NULL),
+        ("Jack", "Black", 5, 1),
+        ("Steve", "Carell", 5, 1),
+        ("Jennifer", "Lawrence", 1, NULL),
+        ("Michael", "Cera", 2, 4),
+        ("Robert", "Downey Jr.", 2, 4),
+        ("Evan", "Peters", 3, 5),
+        ("Hayden", "Christensen", 3, 6),
+        ("Ewan", "McGregor", 6, NULL),
+        ("Channing", "Tatum", 7, NULL),
+        ("George", "Clooney", 8, NULL),
+        ("Chris", "Evans", 8, NULL),
+        ("Margot", "Robbie", 9, NULL),
+        ("Ryan", "Reynolds", 10, 13),
+        ("Scarlett", "Johansson", 10, 13),
+        ("Ryan", "Gosling", 11, 14),
+        ("Will", "Smith", 11, 15),
+        ("Matt", "Damon", 11, 14)
